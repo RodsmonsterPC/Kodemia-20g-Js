4bf1707ced62a45b0b8d470280814c38e02dad62
Rods <rodolfo.perez01@iest.edu.mx>
2022-07-05 11:56:52 -0500

    clase/4

--- Js/string.js
+++ Js/string.js
@@ -2,45 +2,71 @@
 
 //! Lenght
 
-let oracion = "Hallo mein name ist Rodolfo"
+// let oracion = "Hallo mein name ist Rodolfo"
 
-let len = oracion.length
+// let len = oracion.length
 
-console.log(len);
+// console.log(len);
 
 //! replace
 
-const exe = "cat"
+// const exe = "cat"
 
-let rep = exe.replace("c","b")
+// let rep = exe.replace("c","b")
 
-console.log(rep)
+// console.log(rep)
 
 //! IndexOF
 
-const mess = "Wake up, do it"
+// const mess = "Wake up, do it"
 
-let ind = mess.indexOf("ak")
+// let ind = mess.indexOf("ak")
 
-console.log("Index" + ind)
+// console.log("Index" + ind)
 
 //! Lastindex
 
-const ejem = "Paracetamol"
+// const ejem = "Paracetamol"
 
-let lastind = ejem.lastIndexOf("l")
+// let lastind = ejem.lastIndexOf("l")
 
-console.log(lastind);
+// console.log(lastind);
 
 //! StartWidth
 
-const plan = "Java is fun, ha joking"
+// const plan = "Java is fun, ha joking"
 
-let startWidth= plan.startsWith("Java")
+// let startWidth= plan.startsWith("Java")
 
-console.log(startWidth);
+// console.log(startWidth);
 
-startWidth = plan.startsWith("fun")
+// startWidth = plan.startsWith("fun")
 
-console.log(startWidth)
+// console.log(startWidth)
+
+
+//! Practica String
+
+let mensaje = prompt("Escriba una oración")
+
+let largo = mensaje.length
+
+let vocales = ["a","e","i","o","u"]
+
+let contadorVocales = 0;
+
+for (let i of mensaje.toLowerCase()){
+    if (vocales.includes(i)){
+        contadorVocales++;
+    }
+}
+
+let imprimir = mensaje.substring(0, mensaje.length)
+
+console.log(`Tu oración es de ${largo} caracteres, se tiene ${contadorVocales} vocales, ${imprimir}`)
+
+
+
+
+
 
