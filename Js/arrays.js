b1977bcb2a5334015c3c35a797cc27bb6f719ef6
Rods <rodolfo.perez01@iest.edu.mx>
2022-07-11 12:55:57 -0500

    Trabajo semanal

--- Js/arrays.js
+++ Js/arrays.js
@@ -9,23 +9,52 @@
 
 //! shift
 
-let datos = ["HTML", "Javascript" , "Python", "Java"]
+// let datos = ["HTML", "Javascript" , "Python", "Java"]
 
-//¿Cual no es un lenguaje?
+// //¿Cual no es un lenguaje?
 
-let first  = datos.shift();
+// let first  = datos.shift();
 
-console.log(first)
-console.log(datos)
+// console.log(first)
+// console.log(datos)
 
 //! find
 
-let numeros = [1, 3, 4, 7, 9]
+// let numeros = [1, 3, 4, 7, 9]
 
 
-function isEven(element){
-    return element % 2 == 0;
-}
+// function isEven(element){
+//     return element % 2 == 0;
+// }
 
-let resultado = numeros.find(isEven);
-console.log(resultado)
+// let resultado = numeros.find(isEven);
+// console.log(resultado)
+
+//! Ejercicio de la semana
+
+//? Obtener numeros pares de una array
+// let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// if (numeros == ""){
+//     console.log("Se necesita un array")
+// }
+
+// const numerosPares = (num) => {
+
+//     if (num % 2 === 0){
+//         return true;
+
+//     }
+//     else
+//     return false;
+
+// }
+
+// let resultado = numeros.filter(numerosPares)
+
+// console.log(resultado)
+
+//? Obtener los utlimos 2 carcateres de un array
+
+
+
